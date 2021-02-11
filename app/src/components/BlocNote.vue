126586af43731c14ce4c273fa3d94eff32a992c5
Fatymata <fatymata.gassama@gmail.com>
2021-02-11 11:20:37 +0100

    [ADD] update & delete button on notes

--- app/src/components/BlocNote.vue
+++ app/src/components/BlocNote.vue
@@ -3,19 +3,24 @@
     <q-card-section class="bg-purple text-white">
       <div class="text-h6">{{ title }}</div>
     </q-card-section>
-    <q-card-section >
-      
-      <q-card
-        dark
-        bordered
-        class="bg-grey-9 my-card q-ma-sm"
-        v-for="item in notes"
-        :key="item._id"
-      >
-        <q-card-section>
-          <div>{{ item.content }}</div>
-        </q-card-section>
-      </q-card>
+    <q-card-section>
+      <div v-for="item in notes" :key="item._id" class="row">
+        <q-card dark bordered class="bg-grey-9 my-card q-ma-sm col-md-8">
+          <q-card-section>
+            <div>{{ item.content }}</div>
+          </q-card-section>
+        </q-card>
+        <div class="col-md-2">
+          <q-btn flat round class="text-blue" icon="edit" />
+          <q-btn
+            flat
+            round
+            class="text-red"
+            icon="delete"
+            @click="deleteNote"
+          />
+        </div>
+      </div>
     </q-card-section>
     <q-card-actions align="around" v-if="status == 'pending'">
       <q-input outlined dense v-model="text" label="Nouvelle tâche" />
@@ -25,7 +30,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   props: {
     title: String,
@@ -34,35 +39,37 @@ export default {
     update: Function
   },
 
-  data (){
-    return{
-      text : ""
-    }
+  data() {
+    return {
+      text: ""
+    };
   },
 
-  methods : {
-    onSubmit(){
-      //const data = JSON.stringify({"content":this.text});
-      var data = JSON.stringify({ "content" : this.text});
+  methods: {
+    onSubmit() {
+      var data = JSON.stringify({ content: this.text });
 
       var config = {
-        method: 'post',
-        url: 'http://localhost:8080/add',
-        headers: { 
-          'Content-Type': 'application/json'
+        method: "post",
+        url: "http://localhost:8080/add",
+        headers: {
+          "Content-Type": "application/json"
         },
-        data : data
+        data: data
       };
       axios(config)
-      .then(response => {
-        console.log(response.data);
-        this.$emit('update-note');
-        this.text = null
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+        .then(response => {
+          console.log(response.data);
+          this.$emit("update-note");
+          this.text = null;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
 
+    deleteNote() {
+      console.log(mess);
     }
   }
 };
